a8b1879cac11e00cf7ffcd810ba3decfc1c41120
thomasbenard <m.thomas.benard@protonmail.com>
2018-11-25 23:04:37 +0100

    Move body of ReBros.run to Request

--- src/main/java/com/thomasbenard/rebros/ReBros.java
+++ src/main/java/com/thomasbenard/rebros/ReBros.java
@@ -1,10 +1,5 @@
 package com.thomasbenard.rebros;
 
-import java.util.List;
-import java.util.Map;
-
-import static com.thomasbenard.rebros.Matches.emptyResult;
-
 public class ReBros {
     private Content content;
 
@@ -13,18 +8,7 @@ public class ReBros {
     }
 
     public Matches run(Request request) {
-        Matches result = emptyResult();
-        for (String selectField : request.selectedFields()) {
-            List<Node> selectedFields = content.buildMatch().findChildrenMatching(selectField);
-            Map<String, String> whereClauses = request.whereClauses();
-            if (!whereClauses.isEmpty()) {
-                for (Node node : selectedFields) {
-                    if (node.contains(Node.leafNode(whereClauses.get("id"))))
-                        result.put(selectField, node);
-                }
-            } else
-                selectedFields.forEach(match -> result.put(selectField, match));
-        }
-        return result;
+        return request.apply(content.buildMatch());
     }
+
 }
--- src/main/java/com/thomasbenard/rebros/Request.java
+++ src/main/java/com/thomasbenard/rebros/Request.java
@@ -2,24 +2,34 @@ package com.thomasbenard.rebros;
 
 import java.util.*;
 
+import static com.thomasbenard.rebros.Matches.emptyResult;
+
 public class Request {
     private List<String> selectedFields = new ArrayList<>();
-    private Map<String, String> whereClauses = new HashMap<>();
+    private Map<String, Node> whereClauses = new HashMap<>();
 
     public void select(String... field) {
         selectedFields.addAll(Arrays.asList(field));
     }
 
-    List<String> selectedFields() {
-        return selectedFields;
-    }
-
-    Map<String, String> whereClauses() {
-        return whereClauses;
-    }
-
     public void where(String memberName, String value) {
-        whereClauses.put(memberName, value);
+        whereClauses.put(memberName, Node.leafNode(value));
     }
 
+    Matches apply(Node root) {
+        Matches result = emptyResult();
+        for (String selectField : selectedFields) {
+            List<Node> selectedFields = root.findChildrenMatching(selectField);
+            List<Node> matches = new ArrayList<>(selectedFields);
+            Map<String, Node> whereClauses = this.whereClauses;
+            if (!whereClauses.isEmpty()) {
+                for (Node node : selectedFields) {
+                    if (!node.contains(whereClauses.get("id")))
+                        matches.remove(node);
+                }
+            }
+            matches.forEach(match -> result.put(selectField, match));
+        }
+        return result;
+    }
 }
