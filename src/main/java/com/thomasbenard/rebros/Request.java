69709c045660bf52ab21e4fd3620530b768e072b
thomasbenard <m.thomas.benard@protonmail.com>
2018-11-25 23:13:48 +0100

    Refactor Request.apply

--- src/main/java/com/thomasbenard/rebros/Request.java
+++ src/main/java/com/thomasbenard/rebros/Request.java
@@ -1,8 +1,10 @@
 package com.thomasbenard.rebros;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.thomasbenard.rebros.Matches.emptyResult;
+import static com.thomasbenard.rebros.Node.leafNode;
 
 public class Request {
     private List<String> selectedFields = new ArrayList<>();
@@ -13,23 +15,25 @@ public class Request {
     }
 
     public void where(String memberName, String value) {
-        whereClauses.put(memberName, Node.leafNode(value));
+        whereClauses.put(memberName, leafNode(value));
     }
 
     Matches apply(Node root) {
         Matches result = emptyResult();
         for (String selectField : selectedFields) {
             List<Node> selectedFields = root.findChildrenMatching(selectField);
-            List<Node> matches = new ArrayList<>(selectedFields);
-            Map<String, Node> whereClauses = this.whereClauses;
-            if (!whereClauses.isEmpty()) {
-                for (Node node : selectedFields) {
-                    if (!node.contains(whereClauses.get("id")))
-                        matches.remove(node);
-                }
-            }
+            List<Node> matches = filterWhereClauses(selectedFields);
             matches.forEach(match -> result.put(selectField, match));
         }
         return result;
+    }
+
+    private List<Node> filterWhereClauses(List<Node> selectedFields) {
+        if (!whereClauses.isEmpty()) {
+            return selectedFields.stream()
+                    .filter(node -> node.contains(whereClauses.get("id")))
+                    .collect(Collectors.toList());
+        }
+        return selectedFields;
     }
 }
